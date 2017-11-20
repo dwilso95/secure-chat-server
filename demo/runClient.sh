d52eb2393168a387b9d5ad8fa6db0105f4d59c5c
David <dwilso95@jhu.edu>
2017-11-19 20:48:13 -0500

    Added scripts for bash to run demo client/server.

---
+++ demo/runClient.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java --add-modules java.xml.bind -cp bin\SecureChat.jar chat.client.RunClient contexts/client%1.context
---
+++ demo/runServer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java --add-modules java.xml.bind -cp bin\SecureChat.jar chat.server.RunServer contexts/server.context
